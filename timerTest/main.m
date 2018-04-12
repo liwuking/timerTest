20166068e768a3ce22a484b8c858db4e7d8fe980
liwuyang <liwuyang@lbesec.com>
2018-04-12 15:19:27 +0800

    Initial Commit

---
+++ timerTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  timerTest
+//
+//  Created by lbe on 2018/4/12.
+//  Copyright © 2018年 liwuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
